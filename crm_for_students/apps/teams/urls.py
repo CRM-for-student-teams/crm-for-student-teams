78f0567770772334feea9771c2f43394cc0f89e5
whyL1ght <a_toleutayev@kbtu.kz>
2025-12-02 13:40:15 +0500

    quick commit

--- crm_for_students/apps/teams/urls.py
+++ crm_for_students/apps/teams/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
-from .views import UserCreateView, UserDetailView
 
-urlpatterns = [
-    path("create", UserCreateView.as_view(), name="user-create"),
-    path("<int:pk>", UserDetailView.as_view(),
-         name="user-detail"),
-]
