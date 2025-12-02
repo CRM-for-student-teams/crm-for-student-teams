f7b47e9be099e91cc37725e49263288b8638902f
musulmazhamza <musulmazhamza@gmail.com>
2025-12-02 12:47:20 +0500

    added asgi server and channel_redis for websockets

--- crm_for_students/settings/asgi.py
+++ crm_for_students/settings/asgi.py
@@ -1,13 +1,24 @@
 import os
-
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from settings.config import ENV_ID, ENV_POSSIBLE_OPTIONS
+import apps.chat.routing  # correct websocket routes
 
+# Ensure correct environment
+assert ENV_ID in ENV_POSSIBLE_OPTIONS, (
+    f"Set correct DJANGO_ENV_ID env var. Possible options: {ENV_POSSIBLE_OPTIONS}"
+)
 
-assert (
-    ENV_ID in ENV_POSSIBLE_OPTIONS
-), f"Set correct DJANGO_ENV_ID env var. Possible options: {ENV_POSSIBLE_OPTIONS}"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"settings.env.{ENV_ID}")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,  
+    "websocket": AuthMiddlewareStack( 
+        URLRouter(
+            apps.chat.routing.websocket_urlpatterns
+        )
+    ),
+})
