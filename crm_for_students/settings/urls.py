bd763720adb43dc59521091f6da1bbc66ea6877c
musulmazhamza <musulmazhamza@gmail.com>
2025-12-02 10:34:00 +0500

    finished crud

--- crm_for_students/settings/urls.py
+++ crm_for_students/settings/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView
@@ -34,4 +34,5 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("users/", include("apps.teams.urls")),
 ]
